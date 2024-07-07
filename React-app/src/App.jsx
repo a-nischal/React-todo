0c629f4c0e9a2fc10927ca57313e12b08cdacd64
J3rRy0546 <adhikarinischal31@gmail.com>
2024-07-07 13:02:18 +0545

    New Project

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Todos=['Learn html ', 'Learn CSS ', 'learn PHP']
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <form action="">
+      <h1>Hey There!</h1>
+      <input type="text" name="todo" placeholder='New-todo' />
+      {/* <button action="">
+        Submit
+      </button> */}
+      <input type="submit" value={"Add"} />
+    </form>
+      <ul>
+        {Todos.map((todo, index) => {
+          return <li key={index}>{todo}</li>
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
 export default App
