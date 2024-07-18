1d05299ec86f8b051311815c5139d6f159ae3b21
J3rRy0546 <adhikarinischal31@gmail.com>
2024-07-18 15:18:34 +0545

    UseMemo

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -1,19 +1,44 @@
-import "./App.css";
+// import "./App.css";
+import { createContext, useContext } from "react";
 // import Users from "./User";
 // import Weather from "./Weather";
-import Counter from "./Counter";
+// import Counter from "./Counter";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       {/* <Weather /> */}
+//       {/* <Users /> */}
+//       <Counter/>
+//     </>
+//   );
+// }
+
+const UserContext = createContext(null);
+
+const Child = () => {
+  const user = useContext(UserContext);
+  return <h1>{user.name}</h1>;
+};
+
+const Parent = () => {
+  return <Child />;
+};
+
+const GrandParent = () => {
+  return <Parent />;
+};
+
+const hello = () => {
+  const user = { name: "Nischal", age: 21 };
   return (
-    <>
-      {/* <Weather /> */}
-      {/* <Users /> */}
-      <Counter/>
-    </>
+    <UserContext.Provider value={user}>
+      <GrandParent />
+    </UserContext.Provider>
   );
-}
+};
 
-export default App;
+export default hello  ;
 
 // 1. using local storage in todos app
 // 2. useEffect (api call revise)
---
+++ React-app/src/useMemo.jsx
@@ -0,0 +1,34 @@
+import  { useState, useMemo } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+ 
+    const calculateFactorial = (n) => {
+      if (n <= 1) return 1;
+      return n * calculateFactorial(n - 1);
+    };
+   
+   const factorial = useMemo(() => {calculateFactorial(count)}, [count]);
+      return (
+    <div>
+      <h1>useMemo Example</h1>
+      <button onClick={increment}>Count: {count}</button>
+      <p>
+        Factorial of {count} is {factorial}
+      </p>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default App;
